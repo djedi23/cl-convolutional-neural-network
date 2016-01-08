7fe9f57c086bdb923f5f9f7f9cd918e73d85e330
Moïse Valvassori <moise.valvassori@gmail.com>
2016-01-08 18:57:36 +0100

    répète les définitions

--- src/net-definition.lisp
+++ src/net-definition.lisp
@@ -2,48 +2,63 @@
 
 (declaim (optimize (debug 3)))
 
+(defun is-layer (class-name)
+  (let ((class (find-class class-name nil))
+	(layer-class (find-class 'layer)))
+    (if (null class) nil
+	(member layer-class (sb-mop:class-precedence-list class))
+	)))
+
+
 (defgeneric parse-layer-definition (net layer-name def)
-(:documentation "Parse a layer définition and return a list of add layer"))
+  (:documentation "Parse a layer définition and return a list of add layer"))
 
 (defmethod parse-layer-definition ((net symbol) layer def)
-  `((add-layer ,net ,def)))
+  (if (is-layer layer)
+      `((add-layer ,net ,def))
+      (list (flatten0 (mapcar #'(lambda (item)
+				  (if (atom item)
+				      (list item)
+				      (parse-layer-definition net (car item) item)))
+			      def)))
+      ))
 
 (defmethod parse-layer-definition ((net symbol) (layer (eql 'svm)) def)
- (destructuring-bind (layer-name &key num-classes  &allow-other-keys) def
-  `((add-layer ,net ,def)
-    (add-layer ,net (fully-connected ,@(when num-classes `(:num-neurons ,num-classes)))))))
+  (destructuring-bind (layer-name &key num-classes  &allow-other-keys) def
+    `((add-layer ,net ,def)
+      (add-layer ,net (fully-connected ,@(when num-classes `(:num-neurons ,num-classes)))))))
 
 (defmethod parse-layer-definition ((net symbol) (layer (eql 'softmax)) def)
- (destructuring-bind (layer-name &key num-classes  &allow-other-keys) def
-  `((add-layer ,net ,def)
-    (add-layer ,net (fully-connected ,@(when num-classes `(:num-neurons ,num-classes)))))))
+  (destructuring-bind (layer-name &key num-classes  &allow-other-keys) def
+    `((add-layer ,net ,def)
+      (add-layer ,net (fully-connected ,@(when num-classes `(:num-neurons ,num-classes)))))))
 
 #+to-be-implemented
 (defmethod parse-layer-definition ((net symbol) (layer (eql 'regression)) def)
- (destructuring-bind (layer-name &key num-neurons  &allow-other-keys) def
-  `((add-layer ,net ,def)
-    (add-layer ,net (fully-connected ,@(when num-classes `(:num-neurons ,num-neurons)))))))
+  (destructuring-bind (layer-name &key num-neurons  &allow-other-keys) def
+    `((add-layer ,net ,def)
+      (add-layer ,net (fully-connected ,@(when num-classes `(:num-neurons ,num-neurons)))))))
 
 
 (defmethod parse-layer-definition ((net symbol) (layer (eql 'fully-connected)) def)
- (destructuring-bind (layer-name &key activation &allow-other-keys) def
-  `((add-layer ,net ,def)
-    ,(when activation
-        `(add-layer ,net ,(if (listp activation) activation `(,activation))  )))))
+  (destructuring-bind (layer-name &key activation &allow-other-keys) def
+    `((add-layer ,net ,def)
+      ,(when activation
+	     `(add-layer ,net ,(if (listp activation) activation `(,activation))  )))))
 
 
 ;; TODO ajouter le dropout
 
 (defun parse-net-definitions (net defs)
   (flatten0 `(,@(mapcar #'(lambda (def)
-			   (parse-layer-definition net (car def) def))
-		       defs))))
+			    (parse-layer-definition net (car def) def))
+			defs))))
 
 (defmacro define-net ((&key name) &body body)
   (let ((var (gensym (if name (symbol-name name) "NET"))))
     `(let ((,var (net)))
        ,@(parse-net-definitions var body)
-        ,(if name
-           `(setq ,name ,var)
-           var)
+       ,(if name
+	    `(setq ,name ,var)
+	    var)
        )))
--- t/net-definition.lisp
+++ t/net-definition.lisp
@@ -3,23 +3,37 @@
 (plan nil)
 
 #+disabled(print (macroexpand
- '(cnn::define-net (:name example)
-  (input :out-sx 1 :out-sy 1 :out-depth 2)
-  (cnn::svm :num-classes 22)
-  )))
+		  '(cnn::define-net (:name example)
+		    (input :out-sx 1 :out-sy 1 :out-depth 2)
+		    (cnn::svm :num-classes 22)
+		    )))
 
-#-disable(progn
-(cnn::define-net (:name example)
-  (input :out-sx 1 :out-sy 1 :out-depth 2)
-  (cnn::svm :num-classes 22))
-(is (fill-pointer (cnn::layers example)) 3)
-)
+#-disabled
+(progn
+  (cnn::define-net (:name example)
+    (input :out-sx 1 :out-sy 1 :out-depth 2)
+    (cnn::svm :num-classes 22))
+  (is (fill-pointer (cnn::layers example)) 3)
+  )
 
-#-disabled(print (macroexpand
- '(cnn::define-net ()
-  (input :out-sx 1 :out-sy 1 :out-depth 2)
-  (fully-connected :num-neurons 4 :activation (relu :a 4))
-  )))
+#-disabled
+(print (macroexpand
+	'(cnn::define-net ()
+	  (input :out-sx 1 :out-sy 1 :out-depth 2)
+	  (fully-connected :num-neurons 4 :activation (relu :a 4))
+	  )))
 
+#-disabled
+(print (macroexpand
+	'(cnn::define-net ()
+	  (input :out-sx 1 :out-sy 1 :out-depth 2)
+	  (loop for i from 0 below 3
+	       do (progn
+		    (input :out-sx 1 :out-sy 1 :out-depth 2)
+		    (fully-connected :num-neurons 4 :activation (relu :a i))
+		    )))))
+
+
+(is-layer 'svm)
 
 (finalize)
